9dffa74194453be50bd8f8f4d54f9b50b7d64560
Carlos Rodrigues <carlos.rodrigues@sinax.com.br>
2017-04-05 20:34:12 -0300

    Hackerrank 30 days of code - Day 0

--- src/com/hackerrank/carlos/Day0.kt
+++ src/com/hackerrank/carlos/Day0.kt
@@ -1,9 +1,12 @@
 package com.hackerrank.carlos
 
 /**
- * Created by SINAX\carlos.rodrigues on 05/04/17.
+ * Created by carlos.rodrigues on 05/04/17.
  */
 fun main(args: Array<String>){
+    // Print a message on console
     println("Hello, World.")
+
+    // Read a text from console and print a message
     println("${readLine()}")
 }
